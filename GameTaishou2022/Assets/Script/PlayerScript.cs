e6dbfad01d9c059355ce8ba4e789c5221c0f2d40
ariasmiyagi <arias-miyagi-20f@stu.kbc.ac.jp>
2022-02-17 09:37:48 +0900

    teki

---
+++ GameTaishou2022/Assets/Script/PlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+	Rigidbody2D rb;
+	public int moveSpeed = 2;
+
+	void Start()
+	{
+		//GetComponentの処理をキャッシュしておく
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate()
+	{
+		//velocity: 速度
+		//X方向へmoveSpeed分移動させる
+		rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+	}
+}
