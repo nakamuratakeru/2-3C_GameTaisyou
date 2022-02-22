426893b18914c491f9d907a8928f055a730fb600
KBC12005049\kbc12005049 <nanako-tanahara-20f@stu.lbc.ac.jp>
2022-02-22 14:13:51 +0900

    ＨＰバーの実装

---
+++ GameTaishou2022/Assets/Script/HPbar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPbar : MonoBehaviour
+{
+    int maxHp = 100;
+    int currentHp;
+    public Slider slider;
+    bool pushFlag = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Sliderを満タンにする。
+        slider.value = 1;
+        //現在のHPを最大HPと同じに。
+        currentHp = maxHp;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        int damage = 20;
+
+        if (Input.GetKeyDown("space")) //スペースキーを押したら
+        {
+
+            currentHp = currentHp - damage;
+            Debug.Log("ダメージ");
+
+            slider.value = (float)currentHp / (float)maxHp; ;
+        }
+
+    }
+}
