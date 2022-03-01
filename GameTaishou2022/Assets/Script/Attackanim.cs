fb1036a1000056a02982c170c8a91930b416e53a
池原　大樹 <daiki-ikehara-20f@stu.kbc.ac.jp>
2022-03-01 11:24:43 +0900

    攻撃をする際に、それぞれ対応するボタンを押すと、ノーツの位置でエフェクトのアニメーションが再生されるようになった。

---
+++ GameTaishou2022/Assets/Script/Attackanim.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attackanim : MonoBehaviour
+{
+
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("joystick button 0"))
+        {　//Aボタンを押すとAノーツの位置で剣攻撃のアニメーションが流れる
+            animator.SetTrigger("Aslash");
+            Debug.Log("A剣攻撃！");
+        }
+        if (Input.GetKeyDown("joystick button 1"))
+        {　//Bボタンを押すとBノーツの位置で剣攻撃のアニメーションが流れる
+            animator.SetTrigger("Bslash");
+            Debug.Log("B剣攻撃！");
+        }
+        if (Input.GetKeyDown("joystick button 2"))
+        {　//Xボタンを押すとXノーツの位置で魔法攻撃のアニメーションが流れる
+            animator.SetTrigger("XBomb");
+            Debug.Log("X魔法攻撃！");
+        }
+        if (Input.GetKeyDown("joystick button 3"))
+        {　//Yボタンを押すとYノーツの位置で魔法攻撃のアニメーションが流れる
+            animator.SetTrigger("YBomb");
+            Debug.Log("Y魔法攻撃！");
+        }
+    }
+}
