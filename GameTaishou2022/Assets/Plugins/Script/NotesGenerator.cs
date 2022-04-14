7311139f4703956ae73828f1ff68a62a9f9cb478
ariasmiyagi <arias-miyagi-20f@stu.kbc.ac.jp>
2022-04-14 11:00:35 +0900

    1:30秒の譜面

---
+++ GameTaishou2022/Assets/Plugins/Script/NotesGenerator.cs
@@ -0,0 +1,48 @@
+﻿//using UnityEngine;
+//using System;
+
+//public class NotesGenerator : MonoBehaviour {
+//    [Serializable]
+//    public class InputJson {
+//        public Notes[] notes;
+//        public int BPM;
+//    }
+
+//    [Serializable]
+//    public class Notes {
+//        public int num;
+//        public int block;
+//        public int LPB;
+//    }
+
+//    private int[] scoreNum;//ノーツの番号を順に入れる
+//    private int[] scoreBlock;//ノーツの種類を順に入れる
+//    private int BPM;
+//    private int LPB;
+
+//    void Awake() {
+//        MusicReading();
+//    }
+
+//    /// <summary>
+//    /// 譜面の読み込み
+//    /// </summary>
+//    void MusicReading() {
+//        string inputString = scoreData.ToString();
+//        InputJson inputJson = JsonUtility.FromJson<InputJson>(inputString);
+
+//        scoreNum = new int[inputJson.notes.Length];
+//        scoreBlock = new int[inputJson.notes.Length];
+//        BPM = inputJson.BPM;
+//        LPB = inputJson.notes[0].LPB;
+
+//        for (int i = 0; i < inputJson.notes.Length; i++) {
+//            //ノーツがある場所を入れる
+//            scoreNum[i] = inputJson.notes[i].num;
+//            //ノーツの種類を入れる(scoreBlock[i]はscoreNum[i]の種類)
+//            scoreBlock[i] = inputJson.notes[i].block;
+//        }
+
+//    }
+
+//}
