10c03908cf8e674fc60d76673dfc61a67430afc0
nakamuratakeru <takeru-nakamura-20f@stu.kbc.ac.jp>
2022-04-14 11:27:32 +0900

    シーン遷移一通り完成しました

--- GameTaishou2022/Assets/Script/Menu.cs
+++ GameTaishou2022/Assets/Script/Menu.cs
@@ -1,104 +1,21 @@
-﻿//using UnityEngine;
-//using UnityEngine.UI;
-//using UnityEngine.EventSystems;
-//using System.Collections;
-//using System.Collections.Generic;
-
-//public class Menu : MonoBehaviour
-//{
-//    //[SerializeField] private Button pauseButton;
-//    [SerializeField] private GameObject pausePanel;
-//    [SerializeField] private Button resumeButton;
-//    [SerializeField] private GameObject button1, button2, button3;
-//    int startflag = 0;
-//    public bool Opeflg;
-
-//    //public Button FirstSelectButton;
-//    Button Restart;
-//    Button Title;
-//    Button Exit;
-
-//    void Awake()
-//    {
-//        Restart = GameObject.Find("/Menu/exit").GetComponent<Button>();
-//        Title = GameObject.Find("/Menu/title").GetComponent<Button>();
-//        Exit = GameObject.Find("/Menu/Restart").GetComponent<Button>();
-
-//        pausePanel.SetActive(false);
-//        button1.SetActive(false);
-//        button2.SetActive(false);
-//        button3.SetActive(false);
-//        Opeflg = true;
-
-//        //pauseButton.onClick.AddListener(Pause);
-//        //resumeButton.onClick.AddListener(Resume);
-
-//    }
-
-//    void Update()
-//    {
-//        bool start = Input.GetButtonDown("Pause");
-
-
-//        if (start == true && startflag == 0)
-//        {
-
-//            Time.timeScale = 0f;//時間停止
-//            pausePanel.SetActive(true);
-//            button1.SetActive(true);
-//            button2.SetActive(true);
-//            button3.SetActive(true);
-//            Opeflg = false;
-//            Restart.Select();
-//            startflag = 1;
-
-//            Debug.Log("START");
-//            Debug.Log(startflag);
-//        }
-
-//        else if (start == true && startflag == 1)
-//        {
-//            Time.timeScale = 1f;//戻る
-//            pausePanel.SetActive(false);
-//            button1.SetActive(false);
-//            button2.SetActive(false);
-//            button3.SetActive(false);
-//            Opeflg = true;
-//            EventSystem.current.SetSelectedGameObject(null);
-//            startflag = 0;
-
-
-//            Debug.Log("START");
-//            Debug.Log(startflag);
-
-//        }
-//        //Debug.Log(startflag);
-
-//    }
-
-//    //        Time.timeScale = 1;//再開
-//    //        pausePanel.SetActive(false);
-//}
+﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
+using UnityEngine.UI; // UIコンポーネントの使用
 
 public class Menu : MonoBehaviour
 {
+	Button start;
+	Button asobikata;
+	Button title;
 
-    // ボタンが押された場合、今回呼び出される関数
-    void Update()
-    {
-        //if (Input.GetKeyDown("joystick button 1"))
-        //{
-        //    Debug.Log("button1");
-        //}
-        bool Startkettei = Input.GetKeyDown("joystick button 0");
-        if (Startkettei == true)
-        {
-            SceneManager.LoadScene("SampleScene");
-        }
-    }
+	void Start()
+	{
+		// ボタンコンポーネントの取得
+		start = GameObject.Find("/Canvas/START").GetComponent<Button>();
+		asobikata = GameObject.Find("/Canvas/ASOBIKATA").GetComponent<Button>();
+		title = GameObject.Find("/Canvas/BACKTITLE").GetComponent<Button>();
+
+		// 最初に選択状態にしたいボタンの設定
+		start.Select();
+	}
 }
---
+++ GameTaishou2022/Assets/Script/cursorselectmenu.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;//スクリプトでUI(テキストなど)扱うときはこれ必須！！
+using UnityEngine.SceneManagement;
+
+
+public class cursorselectmenu : MonoBehaviour
+{
+    public int It = 0;
+    public int Iz = 0;
+    public int i = 0;
+
+    //private AudioSource audio;
+
+    //[SerializeField]
+    //public AudioClip sound;
+    //public AudioClip sound2;
+
+    //スタート関数
+    void Start()
+    {
+        //audio = gameObject.AddComponent<AudioSource>();
+    }
+
+    //アップデート関数
+    void Update()
+    {
+        float y = Input.GetAxisRaw("Vertical");
+        float x = Input.GetAxisRaw("Horizontal");
+
+        if (y == +1 || y == -1 || x == +1 || x == -1)
+        {
+            if (i == 0)
+            {
+                Puro();
+                i++;
+            }
+        }
+        else
+        {
+            i = 0;
+        }
+
+        if (Input.GetKeyDown("joystick button 0") && It == 0 && Iz == 0)
+        {
+            //Stage1へ移動
+            SceneManager.LoadScene("SampleScene");
+            //Time.timeScale = 1;
+
+            //audio.PlayOneShot(sound2);
+
+        }
+        else if (Input.GetKeyDown("joystick button 0") && It == 0 && Iz == 1)
+        {
+            //Stage2へ移動
+            SceneManager.LoadScene("Asobikata");
+
+            //Time.timeScale = 1;
+
+            //audio.PlayOneShot(sound2);
+
+        }
+        else if (Input.GetKeyDown("joystick button 0") && It == 1 && Iz == 0)
+        {
+            //Stage3へ移動
+            //SceneManager.LoadScene("stage3");
+
+            //audio.PlayOneShot(sound2);
+        }
+        else if (Input.GetKeyDown("joystick button 0") && It == 1 && Iz == 1)
+        {
+            //Stage4へ移動
+            SceneManager.LoadScene("Title");
+            //audio.PlayOneShot(sound2);
+        }
+
+    }
+
+    public void Puro()
+    {
+        float y = Input.GetAxisRaw("Vertical");
+        float x = Input.GetAxisRaw("Horizontal");
+
+        if (y == -1 && It == 0)
+        {
+            transform.position += new Vector3(0, -151, 0);
+            It = 1;
+            //audio.PlayOneShot(sound);
+        }
+        else if (y == +1 && It == 1)
+        {
+            transform.position += new Vector3(0,151, 0);
+            It = 0;
+            //audio.PlayOneShot(sound);
+        }
+
+        if (x == +1 && Iz == 0)
+        {
+            transform.position += new Vector3(370, 0, 0);
+            Iz = 1;
+            //audio.PlayOneShot(sound);
+        }
+        else if (x == -1 && Iz == 1)
+        {
+            transform.position += new Vector3(-370, 0, 0);
+            Iz = 0;
+            //audio.PlayOneShot(sound);
+        }
+    }
+}
