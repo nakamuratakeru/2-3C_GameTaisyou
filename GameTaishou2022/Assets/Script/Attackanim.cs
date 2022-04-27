8695568402d91d6c26dc5fe4b103529da2035e52
池原　大樹 <daiki-ikehara-20f@stu.kbc.ac.jp>
2022-04-27 15:05:31 +0900

    音素材

--- GameTaishou2022/Assets/Script/Attackanim.cs
+++ GameTaishou2022/Assets/Script/Attackanim.cs
@@ -8,10 +8,18 @@ public class Attackanim : MonoBehaviour
     float Xtate_old;
     float Xyoko_old;
     Animator animator;
+    private AudioSource audio;
+
+    [SerializeField]
+    public AudioClip sound;
+    public AudioClip sound2;
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        audio = gameObject.AddComponent<AudioSource>();
+
     }
 
     // Update is called once per frame
@@ -20,14 +28,17 @@ public class Attackanim : MonoBehaviour
         if (Input.GetKeyDown("joystick button 0"))
         {　//Aボタンを押すとAノーツの位置で剣攻撃のアニメーションが流れる
             animator.SetTrigger("Aslash");
+            audio.PlayOneShot(sound);
         }
         if (Input.GetKeyDown("joystick button 1"))
         {　//Bボタンを押すとBノーツの位置で剣攻撃のアニメーションが流れる
             animator.SetTrigger("Bslash");
+            audio.PlayOneShot(sound);
         }
         if (Input.GetKeyDown("joystick button 2"))
         {　//Xボタンを押すとXノーツの位置で魔法攻撃のアニメーションが流れる
             animator.SetTrigger("XBomb");
+            audio.PlayOneShot(sound2);
         }
         //if (Input.GetKeyDown("joystick button 3"))
         //{　//Yボタンを押すとYノーツの位置で魔法攻撃のアニメーションが流れる
@@ -38,6 +49,7 @@ public class Attackanim : MonoBehaviour
         if (Input.GetKeyDown("joystick button 5"))
         {　//RBボタンを押すと魔法の杖のアニメーションが流れる
             animator.SetTrigger("XBomb");
+            audio.PlayOneShot(sound2);
         }
 
         float Xyoko_new = Input.GetAxisRaw("LT");
@@ -48,12 +60,14 @@ public class Attackanim : MonoBehaviour
         { //Aボタンを押すと剣のアニメーションが流れる
             animator.SetTrigger("Aslash");
             Debug.Log("RTキー");
+            audio.PlayOneShot(sound);
         }
 
         if (Xyoko_new == -1 && Xyoko_old==0)
         {　//Bボタンを押すと剣のアニメーションが流れる
             animator.SetTrigger("Bslash");
             Debug.Log("LTキー");
+            audio.PlayOneShot(sound);
         }
         // Xtate == -1
         Xtate_old = Xtate_new;
