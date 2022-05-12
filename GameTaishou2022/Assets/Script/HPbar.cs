58a7f52901a16158e99e18ccf6a2a3a9c820e297
池原　大樹 <daiki-ikehara-20f@stu.kbc.ac.jp>
2022-05-12 14:57:32 +0900

    hpが0になるとゲームオーバーパネルが表示されるようにした。

--- GameTaishou2022/Assets/Script/HPbar.cs
+++ GameTaishou2022/Assets/Script/HPbar.cs
@@ -8,13 +8,16 @@ public class HPbar : MonoBehaviour
 
 
     public int maxHp = 100;
+    [SerializeField] GameObject overPanel;
     //public int currentHp;
     Slider slider;
+    public int dPoint = 0;
     //bool pushFlag = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        overPanel.SetActive(false);
         //slider = GameObject.FindWithTag("HPslider").GetComponent<Slider>();
         slider = GameObject.Find("Slider").GetComponent<Slider>();
 
@@ -29,23 +32,34 @@ public class HPbar : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    //void Update()
-    //{
+    //Update is called once per frame
+    void Update()
+    {
 
-    //    int damage = 20;
+        int damage = 10;
 
-    //    if (Input.GetKeyDown("space")) //スペースキーを押したら
-    //    {
+        if (Input.GetKeyDown("space")) //スペースキーを押したら
+        {
 
-    //        slider.value = slider.value - damage;
-    //        Debug.Log("ダメージ");
+            slider.value = slider.value - damage;
+            Debug.Log("ダメージ");
 
-    //        //slider.value = (float)currentHp / (float)maxHp;
+            //slider.value = (float)currentHp / (float)maxHp;
+
+        }
+        if (slider.value == 0 && dPoint<1)
+        {
+            
+            dPoint += 1;
+            overPanel.SetActive(true);
+            Debug.Log("ゲームオーバー");
+
            
-    //    }
 
-    //}
+        }
+
+
+    }
     public void SetHp(int HP)
     {
         slider.value =HP;
